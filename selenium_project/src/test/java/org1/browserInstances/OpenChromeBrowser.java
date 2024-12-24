05616d3268378fa1316cca4b811d5a06836bd62f
shubhampatil4554 <shubhamspatil2499@gmail.com>
2024-12-24 19:22:35 +0530

    My Selenium Practice Code

---
+++ selenium_project/src/test/java/org1/browserInstances/OpenChromeBrowser.java
@@ -0,0 +1,16 @@
+package org1.browserInstances;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpenChromeBrowser {
+
+	public static void main(String[] args) {
+		//Create an instance of ChromeBrowser class to launch Chrome Browse
+		ChromeDriver sdriver=new ChromeDriver();
+		
+	}
+}
+/*
+ * 1.WebDriver everyTime opens new browser , it won't work on already opened browser
+ * 2.Browser opened by webdriver won't have history cookies and plugins
+*/
